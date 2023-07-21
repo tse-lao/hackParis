0f38a09d8deb7f341844093543859fa72cdd71f0
koenschuite <tse.lao@yahoo.com>
2023-07-21 23:19:48 +0200

    fixed steps for form upload and added nest backend

--- client-app/app/create/page.tsx
+++ client-app/app/create/page.tsx
@@ -10,34 +10,65 @@ import { useState } from "react";
 
 export default function CreateForm() {
     const [activeStep, setActiveStep] = useState(0);
-    
-    
+    const [formData, setFormData] = useState();
+    const [formElements, setFormElements] = useState<any[]>([]);
+    const [image, setImage]  = useState("");
     const nextStep = (step: number) => {
         console.log(step)
         setActiveStep(step);
     }
     
+    const submitForm = async() => {
+        //make sure we can submit form here.. 
+        console.log(formData)
+        console.log(formElements)
+        
+    }
+    
+    const saveForm = (data: any) => {
+        console.log(data);
+        setFormData(data);
+    }
+    
+    function addElement(element:any){
+        setFormElements([...formElements, element]);
+    }
+    function removeElement(id:string){
+        let newElements = formElements.filter((element:any) => element.id !== id);
+        setFormElements(newElements);
+      }
     const steps = [
         {
             name: "Form Details", 
-            component: <FormDetails nextStep={nextStep}/>
+            component: <FormDetails nextStep={nextStep} passData={saveForm}/>
         },
         {
             name: "Form Layout", 
-            component: <FormLayout />
+            component: <FormLayout formElements={formElements} removeElement={removeElement} addElement={addElement}/>
         },
     ]
+
+  
+    const handleImageChange = (e:any) => {
+      if (e.target.files && e.target.files[0]) {
+        let img = URL.createObjectURL(e.target.files[0]);
+        setImage(img);
+      }
+    };
     
   return (
     <div className="flex flex-col items-center justify-center mt-4 p-24">
-        <UploadBanner />
+        <UploadBanner handleImage={handleImageChange} image={image}/>
+        <div className="flex flex-row gap-5 items-center">
         <Steps
             steps={steps}
             activeStep={activeStep}
             nextStep={nextStep}
         />
+        <Button onClick={submitForm}>Submit </Button>
+        </div>
+    
         
-        <Button>Submit </Button>
         <div className="mt-10">
             {steps[activeStep].component}    
         </div>

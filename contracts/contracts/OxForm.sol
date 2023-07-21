35a1507019d6a457f543188f9fc278780908a5d2
George Sarris <nicklioniss@gmail.com>
2023-07-21 21:20:42 +0200

    form contribution function and event added

--- contracts/contracts/OxForm.sol
+++ contracts/contracts/OxForm.sol
@@ -32,23 +32,27 @@ contract OxForm is Ownable, ERC1155, AccessControl {
         address[] requestAdmins
     );
 
+    event ContributionCreated(uint256 formID, string contributionCID, address contributor);
+
+
     Counters.Counter private formID;
 
     mapping(uint256 => FormInfo) private formInfo;
 
+    mapping(uint256 => mapping(address => bool)) private formContributors;
+
     constructor() ERC1155("") {
-        
+
     }
 
-
-    function FormRequest(
+    function formRequest(
         uint256 mintPrice,
         uint256 submitionReward,
         address tokenTreasury,
         EventMetadata memory eventMetadata
-    ) public payable {
+    ) external payable {
 
-        require(submitionReward <= msg.value, "you cannot sponsor any submission");
+        require(submitionReward <= msg.value, "provide more ETH");
 
         formID.increment();
 
@@ -73,8 +77,41 @@ contract OxForm is Ownable, ERC1155, AccessControl {
         );
     }
 
+    function formContribution(
+        uint256 _formID,
+        string calldata contributionCID
+    ) public exists(_formID) {
+        FormInfo storage form = formInfo[_formID];
+        require(!formContributors[_formID][msg.sender], "you can only submit once");
+        require(
+            form.escrowAmount >= form.submitionReward,
+            "no rewards to take"
+        );
+
+        formContributors[_formID][msg.sender] = true;
+        uint reward = form.submitionReward;
+        if (reward > 0) {
+            address payable to = payable(msg.sender);
+            to.transfer(reward);
+        }
+        emit ContributionCreated(_formID, contributionCID, msg.sender);
+    }
+
     function getRequestAdminRole(uint256 _formID) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(_formID, "REQUEST_ADMIN_ROLE"));
+    }
+
+    function hasAccess(address user, uint256 _formID) public view returns (bool) {
+        return balanceOf(user, _formID) > 0;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return formID.current();
+    }
+
+    modifier exists(uint256 _formID) {
+        require(_formID <= formID.current(), "non existed formID");
+        _;
     }
 
     /**

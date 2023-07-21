94aa898582ad11cf536b42142e762efce2407527
koenschuite <tse.lao@yahoo.com>
2023-07-22 00:53:00 +0200

    template to store ocntracts

---
+++ client-app/services/contracts/index.ts
@@ -0,0 +1,24 @@
+
+import goerliFormABI from './abi/goerli/form.json';
+
+
+
+
+const CONTRACTS = {
+    goerli: {
+        FORM: "0x2B452f1e27f43D11dB160ee51b8d4B9D113D6F85", 
+    }, 
+    mumbai: {
+        FORM: ""
+    }
+}
+
+const ABI = {
+    goerli: {
+        form: goerliFormABI,
+    }
+}
+export {
+    ABI, CONTRACTS
+};
+
